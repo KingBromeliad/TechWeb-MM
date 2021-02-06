a304be0cb2e143fb8aa632e37e92822944848ec0
Edoardo Foust <44225702+ForbiddenZone@users.noreply.github.com>
2021-02-06 11:13:56 +0100

    Update index.js
    Aggiunte le route per le nuove views

--- player/src/router/index.js
+++ player/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Storia from '../views/Storia.vue'
+import Quiz from '../views/Quiz.vue'
+import FinalEval from '../views/FinalEval.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +13,21 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/storia',
+    name: 'Storia',
+    component: Storia
+  },
+  {
+    path: '/quiz',
+    name: 'Quiz',
+    component: Quiz
+  },
+  {
+    path: '/final',
+    name: 'FinalEval',
+    component: FinalEval
+  }
   {
     path: '/about',
     name: 'About',
