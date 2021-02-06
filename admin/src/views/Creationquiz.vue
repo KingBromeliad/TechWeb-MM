b6ffd140a59a4ba59dc0135940811714e6cde715
Niro-jpg <62608421+Niro-jpg@users.noreply.github.com>
2021-02-06 11:38:48 +0100

    Add files via upload
    aggiunti file vue

---
+++ admin/src/views/Creationquiz.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <h1>This is the creation page</h1>
+    <button @click="premuto()">premimi</button>
+    <div class="space-y-4">
+    <div v-for="(item,index) in items" :key="index" class="block py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex flex-col sm:items-center sm:space-y-0 sm:space-x-2">
+  <div class="block text-center py-4 space-y-6 sm:items-center">
+    <h1>Le risposte </h1>
+    <div v-for="(item2,index2) in items[index].argomento" :key="index2" class="flex flex-row">
+      <button class="uppercase p-3 flex items-center bg-blue-600 text-blue-50 max-w-max shadow-sm hover:shadow-lg rounded-full w-12 h-12 ">{{index2}}</button>
+    <input type="text" v-model="item.argomento[index2]" class="focus:ring-indigo-500 focus:border-indigo-500 rounded sm:text-sm border-2 border-purple-600" placeholder="aggiunta">
+  </div>
+<button @click="aggiungirisposta(index)" class="px-16 py-2 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Aggiungi risposta</button>
+</div>
+<div class="flex flex-col ">
+  <h1> La domanda</h1>
+    <input type="text" name="company_website" v-model="item.domanda" class="focus:ring-indigo-500 focus:border-indigo-500 rounded sm:text-sm border-2 border-purple-600" placeholder="www.example.com">
+</div>
+<div>
+    <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Elimina {{index}}</button>
+  </div>
+
+</div>
+<div class="block py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+<div class= "sm:items-center text-center space-y-2">
+<button @click="aggiungidomanda()" class="px-16 py-2 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Aggiungi</button>
+<div class="space-y-0.5">
+  <p  class="text-lg text-black font-semibold">
+    Aggiungi una domanda al tuo quiz
+  </p>
+</div>
+</div>
+</div>
+<button @click="salvamodifiche()"> salva modifiche</button>
+</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+export default {
+  data() {
+    return {
+      name:[[]],
+      listastorie:"",
+      items:"",
+    };
+  },
+
+  sockets: {
+    grabjson(data) {
+      this.listastorie=data.storie;
+      console.log(this.listastorie);
+      console.log(this.items);
+    }
+  },
+
+  methods: {
+    premuto() {
+      console.log("mi hai premuto, ahia");
+    },
+    aggiungirisposta(i) {
+    this.items[i].argomento.push(" ");
+  },
+  salvamodifiche() {
+    Vue.prototype.$appName[this.$numerostoria].giochi[this.$numeroquiz].modificato=true;
+    Vue.prototype.$appName[this.$numerostoria].giochi[this.$numeroquiz].opzioni=JSON.parse(JSON.stringify(this.items));
+  },
+  aggiungidomanda() {
+    var a= {
+      domanda: 'un altra domanda',
+      soluzione: 1,
+      argomento:['risposta']
+    }
+    this.items.push(a);
+    console.log(this.items);
+  },
+
+  },
+
+  mounted:function() {
+    console.log("siamo nella quiz creation");
+    console.log(this.$numerostoria);
+    console.log(this.$numeroquiz);
+    console.log(this.$appName);
+    this.items=JSON.parse(JSON.stringify(Vue.prototype.$appName[this.$numerostoria].giochi[this.$numeroquiz].opzioni));
+
+  },
+}
+</script>
---
+++ admin/src/views/Games.vue
@@ -0,0 +1,131 @@
+<template>
+  <div>
+  <div class="block py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex flex-col sm:items-center sm:space-y-0 sm:space-x-2">
+    <div>
+      <div v-if="parolachiave[0]!=null" class="py-8">
+         <button @click="rimuovi(i)" v-for="(item, i) in parolachiave" :key="i" v-bind:id="'A'+i" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            {{item}}
+          </button>
+        </div>
+      <div v-if="parolachiave2[0]!=null">
+        <button @click="inserisci(index)" v-for="(item2,index)  in parolachiave2" :key="index" v-bind:id="'button2'+index" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+          {{item2}}
+        </button>
+          </div>
+        </div>
+        <div>
+          <div v-if="vittoria!=true&&numerodomanda<lunghezza-1">
+          <button @click="avanti()" class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"> avanti</button>
+           </div>
+      <div v-else>
+        <h1>HAI VINTO</h1>
+      <button @click="ricomincia()"> ricomincia</button>
+       </div>
+     </div>
+</div>
+</div>
+
+</template>
+
+<script>
+  //    import myjson from './../DevFile/Domande.json'
+export default {
+  data(){
+    return {
+      parolasegreta:"culo",
+      parolachiave2: [],
+      parolachiave: [],
+      riserva:[],
+      vittoria:false,
+      lunghezzacasi: 20,
+      albero:[],
+      caratteri:'ABCDEFGHIJKLMNOPQRSTUVWYZ',
+      numerodomanda:0,
+      item:["castagna","piemonte"],
+      lunghezza:"",
+    }
+  },
+
+      mounted:function() {
+        this.parolasegreta=this.item[this.numerodomanda];
+        this.azzera();
+        this.lunghezza=this.item.length;
+      },
+
+  sockets: {
+    },
+  methods: {
+    azzera() {
+      this.parolachiave2=[];
+      this.parolachiave=[];
+      this.riserva=[];
+      this.albero=[];
+
+      for (var i = 0; i < this.parolasegreta.length ; i++) {
+          this.parolachiave.splice(i, 1,"X");
+          this.riserva.splice(i, 1,-1);
+      }
+      console.log(this.parolachiave);
+
+      for(var k=0; k<this.lunghezzacasi;k++) {
+        this.parolachiave2.splice(k,1,this.caratteri.charAt(Math.floor(Math.random() * this.caratteri.length)));
+      }
+      for (var j=0; j< this.parolasegreta.length; j++){
+        var vuoto=false;
+        var randomico=Math.floor(Math.random()*19);
+        while(vuoto==false){
+if(this.albero[randomico]!=-1){
+  this.parolachiave2.splice(randomico,1,this.parolasegreta[j]);
+  vuoto=true;
+  this.albero[randomico]=-1;
+}
+else {
+  randomico=Math.floor(Math.random()*19);
+}
+        }
+      }
+console.log(this.parolachiave2)
+          console.log(this.parolasegreta);
+    },
+      rimuovi(i) {
+if (this.parolachiave[i]!='X')     {
+        this.parolachiave2.splice(this.riserva[i],1,this.parolachiave[i]);
+        this.riserva[i]=-1;
+        this.parolachiave.splice(i,1,"X");
+        console.log(this.parolachiave);
+}
+      },
+      inserisci(i) {
+        for (var j = 0; j < this.parolachiave.length ; j++) {
+if(this.parolachiave[j]=="X") {
+  this.parolachiave.splice(j,1,this.parolachiave2[i]);
+      this.riserva.splice(j,1,i);
+    this.parolachiave2.splice(i,1,"X");
+  j=this.parolachiave.length;
+}
+        }
+        var successo=0;
+        for (var k=0; k<this.parolachiave.length; k++) {
+          if (this.parolachiave[k]!=this.parolasegreta[k]){
+            k=this.parolachiave.length;
+            successo=1;
+          }
+        }
+        if(successo!=1) {
+          this.vittoria=true;
+          console.log("vittoria");
+        }
+      },
+      ricomincia() {
+        this.azzera ();
+        this.numerodomanda=0;
+      },
+      avanti() {
+
+        this.numerodomanda++;
+        this.parolasegreta=this.item[this.numerodomanda];
+        this.azzera();
+      }
+  },
+};
+</script>
