c0c6e131492890811d82d87844cc74556d9e67f4
Edoardo Foust <edoardo.foust@studio.unibo.it>
2021-02-10 12:54:09 +0100

    WIP valutatore

---
+++ admin/src/components/PlayerProgress.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <div class="text-center">
+          <h1 class="text-2xl text-black font-bold">{{ player.id }}</h1>
+            <p class="text-base text-gray-500"> 
+               <!-- {{ player.text }} -->
+                Punteggio corrente: {{ player.points }}
+            </p>
+            <div v-if="image != ''">
+                <img class="container" :src=image />
+            </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    player: Object,
+    image: String
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ admin/src/views/SchermataValutazione.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div v-for="(player, index) in this.players" :key="index">
+      <div
+        class="py-8 px-8 max-w-sm mx-auto bg-gray-100 rounded-xl shadow-md space-y-2 sm:py-4 sm:space-y-0 sm:space-x-6 m-1 text-center"
+      >
+        <p class="text-xl text-black align-top">{{ player.id }}</p>
+
+        <button
+          class="align-bottom px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          @click="updateIndex(index)"
+        >
+          Visualizza il progresso del Giocatore
+        </button>
+      </div>
+    </div>
+    <div>
+      <PlayerProgress :image="getImage()" :player="players[shownPlayerIndex]">
+      </PlayerProgress>
+    </div>
+  </div>
+</template>
+
+<script>
+import PlayerProgress from "../components/PlayerProgress.vue";
+export default {
+  name: "SchermataValutazione",
+  components: {
+    PlayerProgress,
+  },
+  data: function () {
+    return {
+      players: [
+        {
+          //nome: '',
+          id: "123",
+          game: "a",
+          points: 0,
+        },
+        {
+          //nome: '',
+          id: "asdasd",
+          game: "a",
+          points: 10,
+        },
+        {
+          //nome: '',
+          id: "hjhjjh",
+          game: "a",
+          points: 15,
+        },
+      ],
+      shownPlayerIndex: 0,
+    };
+  },
+  methods: {
+    getConnectedPlayers: function () {
+      //chiamata all'API per prendere il numero di giocatori
+    },
+    getPlayerPoints: function () {
+      //chiamata al socket al momento del mounted per prendere i progressi
+      this.player_points;
+    },
+    getImage: function () {
+      //chiamata all'API per prendere un immagine e ritorna l'URL come stringa
+      return "";
+    },
+    updateIndex: function (index) {
+      this.shownPlayerIndex = index;
+    },
+  },
+  sockets: {
+    player_points(data) {
+      //this.players = data;
+      console.log(data);
+    },
+  },
+  mounted() {
+    //this.getPlayerPoints();
+  },
+};
+</script>
+
+<style>
+</style>
