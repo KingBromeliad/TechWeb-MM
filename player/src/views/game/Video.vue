0b77e989280ffe80eded4ceb47eaee4fa3735034
Samuele Busi <KingBromeliad@users.noreply.github.com>
2021-02-12 22:56:34 +0100

    Mars tablet component done!

--- player/src/views/game/Video.vue
+++ player/src/views/game/Video.vue
@@ -3,13 +3,32 @@
     class="bg-fixed bg-cover bg-no-repeat min-h-screen"
     v-bind:style="{ 'background-image': background }"
   >
-
-    <div v-show="resourceType" class="grid place-content-center">
-      <iframe :src="source" frameborder="0"></iframe>
-    </div>
-  <div v-show="!resourceType" class="grid place-content-center">
-      <img :src="source"/>
+ 
+    <div class="grid place-content-center h-screen">
+       <div class="flex justify-center align-center text-center mb-4">
+    <h1 class="text-black md:text-6xl text-2xl font-extrabold">{{title}}</h1>
   </div>
+      <iframe
+        width="720"
+        height="400"
+        :src="source"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <div class="flex justify-center">
+        <button
+          @click="Continue()"
+          class="bg-black hover:bg-gray-700 focus:outline-none rounded-lg font-bold text-center text-white md:text-2xl sm:text-xl p-2 mt-4"
+        >
+          Continue
+        </button>
+      </div>
+    </div>
+
+    <div v-show="!resourceType" class="grid place-content-center h-screen">
+      <img :src="source" />
+    </div>
   </body>
 </template>
 <script>
@@ -18,17 +37,32 @@ export default {
     data: Object,
   },
   computed: {
+    title: function(){
+      return this.data.title[this.line]
+    },
     source: function() {
-      return ('http:localhost:3500/' + this.data.resource[0]);
+      return this.data.source[this.line];
     },
     background: function() {
       return "url(http://localhost:3500/" + this.data.images.background + ")";
     },
-    resourceType: function(){
-      return this.data.resourceType
+    resourceType: function() {
+      return this.data.resourceType;
+    },
+  },
+  data: function(){
+    return{
+      line: 0
     }
   },
   methods: {
+    Continue() {
+      if (this.line < this.data.source.length - 1) {
+        this.line++;
+      } else if (this.line == this.data.source.length - 1) {
+        this.ContinueToNext();
+      }
+    },
     ContinueToNext() {
       this.$emit("gameCompleted");
     },
