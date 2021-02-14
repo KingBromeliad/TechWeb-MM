7503f5d576deb21a2b739741eae41794d6a0f23c
KingBromeliad2 <busi.samuele99@gmail.com>
2021-02-14 15:39:45 +0100

    path game now working

--- player/src/components/Rock.vue
+++ player/src/components/Rock.vue
@@ -1,18 +1,24 @@
 <template>
-  <div 
+  <div
     :class="rotation"
     @click="Selected()"
     class="flex items-center justify-items-center transform"
   >
-    <img :class="highlight" class="object-contain rounded-xl" src="http://localhost:3000/Rock.png" alt="rock" />
+    <img
+      :class="highlight"
+      class="object-contain rounded-xl"
+      :src="image"
+      alt="rock"
+    />
   </div>
 </template>
 <script>
 export default {
   name: "rock",
   props: {
-      initialAngle : Number,
-      rotates: Boolean
+    initialAngle: Number,
+    rotates: Boolean,
+    id: Number,
   },
   data: function() {
     return {
@@ -23,14 +29,19 @@ export default {
     };
   },
   computed: {
-    highlight: function(){
-      if (this.high_light) return 'bg-green-300';
-      else return '';
+    image: function() {
+      if (this.id == 1 || (this.id == 16))
+        return "http://localhost:3000/RockArrow.svg";
+      else return "http://localhost:3000/Rock.png";
+    },
+    highlight: function() {
+      if (this.high_light) return "bg-green-300";
+      else return "";
     },
     rotation: function() {
       let x;
       let y;
-      y = this.initialAngle + this.angle +1;
+      y = this.initialAngle + this.angle + 1;
       switch (y) {
         case 0:
           x = "rotate-0";
@@ -45,23 +56,24 @@ export default {
           x = "-rotate-90";
           break;
         case 4:
-            x = "rotate-0"
+          x = "rotate-0";
       }
-                return x;
+      return x;
     },
   },
 
   methods: {
-    Selected(){
+    Selected() {
       this.Rotate();
-      this.high_light = !this.high_light
+      this.high_light = !this.high_light;
+      this.$emit("rockClick", this.id, this.high_light);
     },
     Rotate() {
-      if (this.rotates && !this.rotated){
-      if (this.clicked) this.angle -= 1;
-      else this.angle += 1;
-      this.clicked = !this.clicked;
-      this.rotated = true;
+      if (this.rotates && !this.rotated) {
+        if (this.clicked) this.angle -= 1;
+        else this.angle += 1;
+        this.clicked = !this.clicked;
+        this.rotated = true;
       }
     },
   },
